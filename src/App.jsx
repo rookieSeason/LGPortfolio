d9efb5d820cf7aa4a7b2a3de026a4585b8be93e7
Lester <gatpolintanjohn@gmail.com>
2024-01-17 14:34:20 +0800

    screen adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import Nav from './components/Nav';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Experience from './components/Experience';
-
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Experience from "./components/Experience";
 
 import React from "react";
 
 function App() {
-
   return (
-    <div className='font-google'>
+    <div className="font-google overflow-hidden overflow-x-none">
       <Nav />
       <Home />
       <About />
       <Experience />
       <Projects />
       <Contact />
-      <Footer />      
+      <Footer />
     </div>
   );
-};
+}
 
-export default App
+export default App;
